f7ba6a7dc0476468e9d507af73a68f6531e7c9ce
wadoodalam <wadoodalam@outlook.com>
2024-01-17 01:21:05 -0800

    Data Profile Created

--- data_profile.py
+++ data_profile.py
@@ -1,11 +1,23 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-# missing values are represented by 99/-99 in the dataset
-data = pd.read_csv('cfhtlens.csv', na_values=[99,-99])
+def Graph(data):
+    data.hist()
+    plt.savefig('histogram.png')
+
+def Describe(data):
+    description = data.describe()
+    description = description.loc[['min', 'max', 'mean', '50%','count']]
+    description.to_csv('profile.csv')
 
 
 
-description = data.describe()
-description = description.loc[['min', 'max', 'mean', '50%','count']]
-description.to_csv('profile.csv')
+if __name__ == "__main__":
+
+    # missing values are represented by 99/-99 in the dataset
+    data = pd.read_csv('cfhtlens.csv', na_values=[99,-99])
+
+    #Describe(data) # mean, median, max, min, num_of_missing values
+    #Graph(data) # Create Histogram
+    
