c070f06297ad4dbe14961ed0b8ba9bbd4ad530c1
wadoodalam <wadoodalam@outlook.com>
2024-01-24 12:49:42 -0800

    Accuracy for missing data points is added

--- train_eval.py
+++ train_eval.py
@@ -18,11 +18,12 @@ def TrainTestSplit(X,Y, omitFeatures=False):
     if omitFeatures:
         # drop col with missing values
         X = X.dropna(axis=1,how='any')
+        
       
     missing = np.isnan(X).any(axis=1)
     # The test data does not include rows with missing values
     X_train, X_test, Y_train, Y_test = train_test_split(X[~missing], Y[~missing], train_size=3000, random_state=0)
-    #print("Training set count:", len(Y_train))
+    
 
     # Concatenate the missing value rows in the test set
     X_test = pd.concat([X_test, X[missing]])
@@ -30,67 +31,84 @@ def TrainTestSplit(X,Y, omitFeatures=False):
 
     return X_train, X_test, Y_train, Y_test
 
+
 def TrainRandomForest(X_train, Y_train):
-    classifier = RandomForestClassifier(random_state=0)
+    # random_state=0,min_samples_split=10,n_estimators=100
+    classifier = RandomForestClassifier(random_state=0,min_samples_split=10,n_estimators=100)
     classifier.fit(X_train, Y_train)
+   #print('Parameters of RF:', classifier.get_params())
+    
     return classifier
 
 
-def Predict(classifier, X_test, Y_test, strategy):
+def Predict(strategy):
+
+    X,Y = LoadData()
+    X_train, X_test, Y_train, Y_test = TrainTestSplit(X,Y)
+    classifier = TrainRandomForest(X_train,Y_train)
+    missing = np.isnan(X_test).any(axis=1)
     if strategy == 'abstain':
-        missing = np.isnan(X_test).any(axis=1)
+        
         Y_predict = classifier.predict(X_test[~missing])
         accuracy = len(Y_predict) / len(Y_test)
-        print('Number if items missing are =', np.sum(missing))
+        missing_accuracy = 0
+        #print('Number if items missing are =', np.sum(missing))
     
     if strategy == 'majorityClass':
-        # Get the mode from test set, use the first value in the array
+        # Get the mode from train set, use the first value in the array
         mode_class = Y_train.mode().iloc[0]
         # Create and fill in the predict np array using that mode
         Y_predict = np.full(len(Y_test), mode_class)
         accuracy = accuracy_score(Y_test, Y_predict)
+        missing_accuracy = 99
     
     if strategy == 'omitFeatures':
-        # Loading data again as we need to omit features
-        X,Y = LoadData()
         # with omitFeatures set to true, getting the split accordingly
         X_train_omit, X_test_omit, Y_train_omit, Y_test_omit = TrainTestSplit(X,Y,omitFeatures=True)
+
         #Re-train the classifier
         classifier = TrainRandomForest(X_train_omit,Y_train_omit)
-        # Prediction and accuracy
         Y_predict = classifier.predict(X_test_omit)
+
+        # need to convert to numpy, and treat as type int due to misalignment issues due to dropping features
+        Y_test_omit = Y_test_omit.to_numpy()
+        Y_predict = Y_predict.astype(int)
+
+        # accuracy
+        missing_accuracy = accuracy_score(Y_test_omit[missing], Y_predict[missing])
         accuracy = accuracy_score(Y_test_omit, Y_predict)
     
     if strategy == 'imputeAvg':
-        X,Y = LoadData()
-        X_train, X_test, Y_train, Y_test = TrainTestSplit(X,Y)
        
         imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
         imputer.fit(X_train)
         imputed_data = imputer.transform(X_test)
         
         Y_predict = classifier.predict(imputed_data)
+
+        missing_accuracy = accuracy_score(Y_test[missing], Y_predict[missing])
         accuracy = accuracy_score(Y_test,Y_predict)
 
     if strategy == 'imputeNeighbor':
-        X,Y = LoadData()
-        X_train, X_test, Y_train, Y_test = TrainTestSplit(X,Y)
 
-        imputer = KNNImputer()   
+        imputer = KNNImputer(n_neighbors=5)   
         imputer.fit(X_train)
         imputed_data = imputer.transform(X_test)
         
         Y_predict = classifier.predict(imputed_data)
+
+        missing_accuracy = accuracy_score(Y_test[missing], Y_predict[missing])
         accuracy = accuracy_score(Y_test,Y_predict)
 
 
-    return accuracy
+    return accuracy, missing_accuracy
 
 if __name__ == "__main__":
 
-    X,Y = LoadData()
-    X_train, X_test, Y_train, Y_test = TrainTestSplit(X,Y)
-    classifier = TrainRandomForest(X_train,Y_train)
     
-    accuracy = Predict(classifier, X_test, Y_test, 'imputeNeighbor')
-    print(f'Accuracy: {accuracy:.4f}')
+    strategies = ['abstain','majorityClass', 'omitFeatures','imputeAvg','imputeNeighbor']
+    strategyAccuracy = []
+    for strategy in strategies: 
+        accuracy,missing_accuracy = Predict(strategy)
+        strategyAccuracy.append((strategy,accuracy,missing_accuracy))
+    print(strategyAccuracy)
